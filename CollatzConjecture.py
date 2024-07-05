643779216029484f7a8ae0b01d589738e117e203
nion <167205269+Noulion@users.noreply.github.com>
2024-07-05 09:52:11 +0800

    Add files via upload

---
+++ CollatzConjecture.py
@@ -0,0 +1,35 @@
+#collatz conjecture by @Noulion
+import time 
+
+
+#conjecture-calculator
+def cc(x):
+    print(x)
+    
+    #Calcultion loop
+    while 1==1:
+        time.sleep(0.02)
+        
+        #if x is less than or equal to 0
+        if x <= 0:
+            break
+        #if x is not even then do 3x+1
+        if (x % 2) != 0:
+            x = x*3+1
+            print(int(x))
+        #if x is even then divide by 2
+        else:
+            x = x / 2
+            print(int(x))
+                     
+cc(x) #please change x to a number value
+
+"""
+The Collatz Conjecture, also known as the 
+"3n + 1" sequence, proposes that starting with any positive number and applying two rules 
+(if even, divide by two; if odd, triple it and add one) will always eventually lead to these following outcomes:
+    0
+    1-4-2 loop
+
+src:https://science.howstuffworks.com/math-concepts/collatz-conjecture.htm
+"""
